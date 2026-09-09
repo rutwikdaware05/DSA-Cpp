64c2305c311c1d2926905819a91cdb39ce7d7443
Rutwik Daware <rutwikdaware123@gmail.com>
2026-09-09 23:13:02 +0530

    Time: 0 ms (100%), Space: 12.4 MB (92.94%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> res;
+        int maxi = *max_element(candies.begin(),candies.end());
+        for(int x  : candies){
+            if(x + extraCandies >= maxi){
+                res.push_back(true);
+            }
+            else{
+                res.push_back(false);
+            }
+        }
+        return res;
+    }
+};
